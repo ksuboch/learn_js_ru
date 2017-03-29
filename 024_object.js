e1bf164257978b733593c318eacc9724a25741c2
Kirill Suboch <3dvapper.inbox@gmail.com>
2017-03-29 15:53:46 +0400

    024. Object

---
+++ 024_object.js
@@ -0,0 +1,11 @@
+// Создайте пустой объект user.
+// Добавьте свойство name со значением Вася.
+// Добавьте свойство surname со значением Петров.
+// Поменяйте значение name на Сергей.
+// Удалите свойство name из объекта
+
+var user = {};
+user.name = "Vasia";
+user.surname = "Petrov";
+user.name = "Sergey";
+delete user.name;
