b9bf7222a4c3e712399339cc075f7ad5ef03c920
Kirill Suboch <3dvapper.inbox@gmail.com>
2017-03-30 15:10:30 +0400

    027. Array methods

---
+++ 027_array_methods.js
@@ -0,0 +1,127 @@
+var obj = {
+	className: "open menu"
+}
+
+// создайте функцию addClass(obj, cls) которая добавляет в список
+// cclassName имя класса, если его там ещё нет
+
+function addClass(obj, cls) {
+	// if (!!obj.className.indexOf(cls)) {
+	// 	var className = obj.className.split(" ");
+	// 	className.push(cls);
+	// 	obj.className = className.join(" ");
+	// }
+
+	var classNames = obj.className ? obj.className.split(" ") : [];
+
+	for (var i = 0; i < classNames.length; i++) {
+		if (classNames[i] == cls) return;
+	}
+
+	classNames.push(cls);
+
+	obj.className = classNames.join(" ");
+}
+
+addClass(obj, "new");
+addClass(obj, "open");
+addClass(obj, "me");
+
+console.log(obj);
+
+// напишите функцию camelizre(str) которая преобразует строки вида 
+// "my-short-string" в "myShortString"
+
+function camelize(str) {
+	var parts = str.split("-");
+	for (var i = 1; i < parts.length; i++) {
+		if (parts[i]) {
+			parts[i] = parts[i][0].toUpperCase() + parts[i].slice(1);
+		}
+	}
+	return parts.join("");
+}
+
+console.log(camelize("background-color") == 'backgroundColor');
+console.log(camelize("list-style-image") == 'listStyleImage');
+console.log(camelize("-webkit-transition") == 'WebkitTransition');
+
+// напишите функцию removeClass, которая удаляет класс cls, если он есть
+
+function removeClass(obj, cls) {
+	var classNames = obj.className ? obj.className.split(" ") : [];
+
+	var i = 0;
+	while (i < classNames.length) {
+		if (classNames[i] == cls) {
+			classNames.splice(i, 1);
+		} else {
+			i++;
+		}
+	}
+
+	obj.className = classNames.join(" ");
+}
+
+obj = {
+  className: 'my menu menu'
+};
+
+removeClass(obj, 'menu');
+console.log( obj.className );
+
+// создать функцию filterRangeInPlace(arr, a, b), котрая получает массив
+// с числами и удаляет из него все числа вне диапазона a..b
+
+function filterRangeInPlace(arr, a, b) {
+	var i = 0;
+	while (i < arr.length) {
+		if (a <= arr[i] && arr[i] <= b) {
+			i++;
+		} else {
+			arr.splice(i, 1);
+		}
+	}
+}
+
+arr = [5, 3, 8, 1];
+
+filterRangeInPlace(arr, 1, 4);
+console.log( arr );
+
+// как отсортировать массив в обратном порядке
+
+var arr = [5, 2, 1, -10, 8];
+console.log(arr);
+
+// как отсортировать массив в обратном порядке
+
+var arr = [5, 2, 1, -10, 8];
+arr.sort(function(a, b) {
+	return -(b - a);
+})
+
+console.log(arr);
+
+// скопируйте и отсортируйте массив
+
+var arr = ["HTML", "JavaScript", "CSS"];
+
+arrSorted = arr.slice().sort();
+console.log(arrSorted);
+
+// отсортируйте массив в случайном порядке
+
+var arr = [1, 2, 3, 4, 5];
+
+console.log(arr.sort(function() {return Math.random() - 0.5}));
+
+// отсортируйте массив объектов people по полю age
+
+var vasya = { name: "Вася", age: 23 };
+var masha = { name: "Маша", age: 18 };
+var vovochka = { name: "Вовочка", age: 6 };
+
+var people = [ vasya , masha , vovochka ];
+
+console.log(people.sort(function(a, b) {return a.age - b.age}));
