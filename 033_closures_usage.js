2f04af552e85dfbb23ff01793419223187148b18
Kirill Suboch <3dvapper.inbox@gmail.com>
2017-04-05 10:05:49 +0400

    033. Closures usage (part 2)

--- 033_closures_usage.js
+++ 033_closures_usage.js
@@ -108,3 +108,43 @@ users.sort(byField('age'));
 users.forEach(function(user) {
   console.log(user.name);
 }); // Маша, Вася, Петя
+
+// Создайте функцию filter(arr, func), которая получает массив arr 
+// и возвращает новый, в который входят только те элементы arr, 
+// для которых func возвращает true.
+// Создайте набор «готовых фильтров»: inBetween(a,b) – «между a,b», 
+// inArray([...]) – "в массиве [...]". Использование должно быть таким:
+// filter(arr, inBetween(3,6)) – выберет только числа от 3 до 6,
+// filter(arr, inArray([1,2,3])) – выберет только элементы, 
+// совпадающие с одним из значений массива.
+
+function filter(arr, func) {
+	var tmp = [];
+	for (var i = 0; i < arr.length; i++) {
+		if (func(arr[i])) {
+			tmp.push(arr[i]);
+		}
+	}
+	return tmp;
+}
+
+function inBetween(a, b) {
+	return function(x) {
+		return (a <= x) && (x <= b);
+	}
+}
+
+function inArray(arr) {
+	return function(x) {
+		return arr.indexOf(x) != -1;
+	}
+}
+
+var arr = [1, 2, 3, 4, 5, 6, 7];
+
+console.log(filter(arr, function(a) {
+  return a % 2 == 0
+})); // 2,4,6
+
+console.log(filter(arr, inBetween(3, 6))); // 3,4,5,6
+console.log(filter(arr, inArray([1, 2, 10]))); // 1,2
